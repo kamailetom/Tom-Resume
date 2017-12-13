ed9ad52c20f5a19c1a688ab602eda7e75cb17e6a
kamailetom <TomCarr@purplemaia.org>
2017-12-13 11:12:16 -1000

    Added index.html, js files, and problemset.html
    Added index.html, helper.js, jQuery.js, resumeBuilder.js, problemSet.html

---
+++ resumeBuilder.js
@@ -0,0 +1,27 @@
+/*
+This is empty on purpose! Your code to build the resume will go here.
+ */
+var skills = ["Programming"," teaching", " JS"]
+
+var awesomeThoughts = "I am Tom and I am awesome"
+
+var funThoughts = awesomeThoughts.replace("awesome", "fun");
+
+var firstName = "Tom";
+
+var role = "Web Developer";
+
+var age= 15;
+
+var formattedName = HTMLheaderName.replace("%data%", firstName);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role );
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
+
+
+
+
+console.log(awesomeThoughts)
+
